b59ba8721da6bd00ba617efcbf8e885e43acdca3
Aaron Korte <aaron.korte@stud.uni-goettingen.de>
2026-05-30 19:30:53 +0200

    adding logging levels to functions

--- weather_API.py
+++ weather_API.py
@@ -1,20 +1,42 @@
 import requests
 from dotenv import load_dotenv
 import os
+import logging
+import mysql.connector
 
-load_dotenv()  # Loads the .env file
+# Loading .env file
+load_dotenv()
 
+# Basic configuration: log level, format, and output destination
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s', # Specifies the log message format.
+    handlers=[
+        logging.FileHandler("pipeline.log"),  # Log to a file named "pipeline.log"
+        logging.StreamHandler()               # Log to console
+    ]
+)
 
+logger = logging.getLogger(__name__) # creates a logger object unique to the current module.
+logger.info("Pipeline starts...")
+
+# function to retrieve coordinates
 def get_geocoding(city, country_code, limit= 1):
     # Input validation
     if not city or not country_code:
-        raise ValueError("City and country code must be provided")
+        logger.info("City and country code are not provided.")
+        raise ValueError("City and country code must be provided.")
+
     if not isinstance(limit, int) or limit <= 0:
+        logger.info("Limit is not an integer greater than 0.")
         raise ValueError("Limit must be an integer greater than 0")
+
+    logger.info(f"Fetching geocoding for city: {city}, country: {country_code}")
 
     # Loading API key
     api_key = os.getenv("api_key")
     if not api_key:
+        logger.error(f"API key not found.")
         raise ValueError("API key not found.")
 
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{country_code}&limit={limit}&appid={api_key}"
@@ -26,13 +48,15 @@ def get_geocoding(city, country_code, limit= 1):
         if data:
             return data[0]['lat'], data[0]['lon']
         else:
+            logger.info("No coordinates found.")
             raise ValueError("No coordinates found.")
     except requests.exceptions.RequestException as e:
+        logger.error(f"Error: {e}")
         print(f"Error: {e}")
         return None
 
 
-
+# function to retrieve weather data
 def get_weather(lat, lon):
     if not lat or not lon:
         raise ValueError("Latitude and longitude coordinates must be provided.")
@@ -40,6 +64,7 @@ def get_weather(lat, lon):
         # Loading API key
     api_key = os.getenv("api_key")
     if not api_key:
+        logger.error(f"API key not found.")
         raise ValueError("API key not found.")
 
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
@@ -51,6 +76,98 @@ def get_weather(lat, lon):
         print(f"Error: {e}")
         return None
 
+lat, lon = get_geocoding("Auckland", "NZ")
+weather_data = get_weather(lat, lon)
+
+
+# function to get needed data from API call
+def select_data(data):
+    selected_data = {
+        "id": data["id"],
+        "timestamp": data["dt"],
+        "timezone": data["timezone"],
+        "windspeed": data["wind"]["speed"],
+        "country": data["sys"]["country"],
+        "city": data["name"],
+        "humidity": data["main"]["humidity"],
+        "pressure": data["main"]["pressure"],
+        "temp": data["main"]["temp"],
+        "temp_max": data["main"]["temp_max"],
+        "temp_min": data["main"]["temp_min"]
+    }
+    return selected_data
+
+
+
+
+# function to connect to database(MySQL)
+def db_connect():
+    # Load file with credentials to log into DB
+    logger.info(f"Loading credentials.")
+    load_dotenv()
+
+    try:
+        logger.info("Trying to connect to the database...")
+        db = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME')
+        )
+        logger.info("Successfully connected to the database.")
+    except mysql.connector.Error as err:
+        logger.error(f"Database connection error: {err}")
+        raise
+
+def execute_query(db, query, params=None):
+    try:
+        with db.cursor() as cursor:
+            logger.info(f"Executing query: {query}")
+            cursor.execute(query, params or ())
+            result = cursor.fetchall()
+            logger.info(f"Query executed successfully.")
+            return result
+    except mysql.connector.Error as err:
+        logger.error(f"Database query error: {err}")
+        raise
+
+
+# SQL query to check if table exists
+current_weather_table = """
+    CREATE TABLE IF NOT EXISTS current_weather (
+        id SERIAL PRIMARY KEY,
+        timestamp TIMESTAMP,
+        timezone INTEGER,
+        windspeed FLOAT,
+        country VARCHAR(50),
+        city VARCHAR(50),
+        humidity INTEGER,
+        pressure INTEGER,
+        temp FLOAT,
+        temp_max FLOAT,
+        temp_min FLOAT
+)
+"""
+# SQL query to insert data
+current_weather_insert = """
+INSERT INTO current_weather (id, timestamp, timezone, windspeed, country, city, humidity, pressure, temp, temp_max, temp_min)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s)
+"""
+
+# extracting values
+params = (
+    weather_data["id"],
+    weather_data["timestamp"],
+    weather_data["timezone"],
+    weather_data["windspeed"],
+    weather_data["country"],
+    weather_data["city"],
+    weather_data["humidity"],
+    weather_data["pressure"],
+    weather_data["temp"],
+    weather_data["temp_max"],
+    weather_data["temp_min"]
+)
 
 
 
